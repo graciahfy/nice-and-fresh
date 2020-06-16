51162d7734daff3e8cbf8c199eaacb9691e74898
orange breeze <31154472+hufengyi11@users.noreply.github.com>
2020-06-16 12:10:27 +0100

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -1,3 +1,7 @@
+#Q1
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
 n = 1
 list = []
 while n < 1000:
@@ -6,6 +10,4 @@ while n < 1000:
     elif n%5 == 0:
         list. append(n)
     n = n + 1
-
-
 print(sum(list))
